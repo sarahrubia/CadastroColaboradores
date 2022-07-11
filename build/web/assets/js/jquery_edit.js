754412dcc41ef49ebca3bdb46f3045f62fba186b
sarahrubia <sarahrubia22@gmail.com>
2022-07-11 15:39:24 -0300

    update final

---
+++ build/web/assets/js/jquery_edit.js
@@ -0,0 +1,14 @@
+// Marcando radiobuttons no formEditarColaborador.jsp;
+
+$(document).ready(function(){
+
+    var cpfValue = $('.cpfEdit').val();
+    if (cpfValue != "") {
+        $("#pessoaFisica").prop("checked", true).change();
+        $('.boxcpf').show();      
+    } else {
+        $("#pessoaJuridica").prop("checked", true).change();
+        $('.boxcnpj').show();
+    };
+  
+  });
